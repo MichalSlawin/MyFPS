db5f173bf290a15d07c9635644df85a741cdf343
Michal Slawinski <michal.slawin@gmail.com>
2021-04-17 14:13:46 +0200

    placed gun model, started shooting script

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    [SerializeField] private Camera fpsCamera;
+    [SerializeField] private float damage = 10f;
+    [SerializeField] private float range = 100f;
+
+    void Update()
+    {
+        HandleInput();
+    }
+
+    private void HandleInput()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(fpsCamera.transform.position, fpsCamera.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+        }
+    }
+}
