83af90d6de9be5623f58e4616c6b8f51284bc6c6
Michal Slawinski <michal.slawin@gmail.com>
2021-04-18 20:50:16 +0200

    weapons ammo and reloading

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -1,34 +1,76 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class Gun : MonoBehaviour
 {
+    private const int MAX_MAGAZINES = 10;
+
     [SerializeField] private Camera fpsCamera = null;
     [SerializeField] private float damage = 10f;
     [SerializeField] private float range = 100f;
     [SerializeField] private float fireRate = 15f;
-    [SerializeField] private int ammo = 15;
+    [SerializeField] private int maxAmmo = 15;
+    [SerializeField] private float reloadTime = 1f;
+    [SerializeField] private int startMagazines = 5;
     [SerializeField] private ParticleSystem gunShotParticle = null;
     [SerializeField] private ParticleSystem impactEffectPrefab = null;
+    [SerializeField] private Animator animator = null;
 
     private float nextTimeToFire = 0f;
     private AudioSource audioSource;
+    private int currentAmmo;
+    private int currentMagazines;
+    private bool reloading = false;
+
+    private void OnEnable()
+    {
+        reloading = false;
+        animator.SetBool("reloading", false);
+        UIController.SetAmmoText(currentAmmo, currentMagazines);
+    }
 
     private void Start()
     {
         audioSource = GetComponent<AudioSource>();
+        currentAmmo = maxAmmo;
+        currentMagazines = startMagazines;
+        UIController.SetAmmoText(currentAmmo, currentMagazines);
     }
 
     void Update()
     {
+        if(currentAmmo <= 0 && !reloading && currentMagazines > 0)
+        {
+            StartCoroutine(Reload());
+        }
         HandleInput();
+    }
+
+    private IEnumerator Reload()
+    {
+        reloading = true;
+        yield return new WaitForSeconds(0.25f);
+        animator.SetBool("reloading", true);
+        yield return new WaitForSeconds(reloadTime - 0.5f);
+        animator.SetBool("reloading", false);
+        yield return new WaitForSeconds(0.25f);
+        currentAmmo = maxAmmo;
+        currentMagazines--;
+        reloading = false;
+        UIController.SetAmmoText(currentAmmo, currentMagazines);
     }
 
     private void HandleInput()
     {
-        if(Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
+        if(Input.GetButton("Fire1") && Time.time >= nextTimeToFire && !reloading && currentAmmo > 0)
         {
             nextTimeToFire = Time.time + 1f / fireRate;
             Shoot();
+        }
+
+        if (Input.GetKeyDown(KeyCode.R) && !reloading && currentAmmo != maxAmmo && currentMagazines > 0)
+        {
+            StartCoroutine(Reload());
         }
     }
 
@@ -38,8 +80,10 @@ public class Gun : MonoBehaviour
         RaycastHit hit;
 
         audioSource.Play();
+        currentAmmo--;
+        UIController.SetAmmoText(currentAmmo, currentMagazines);
 
-        if(Physics.Raycast(fpsCamera.transform.position, fpsCamera.transform.forward, out hit, range))
+        if (Physics.Raycast(fpsCamera.transform.position, fpsCamera.transform.forward, out hit, range))
         {
             if(hit.transform.CompareTag("Enemy"))
             {
---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class UIController : MonoBehaviour
+{
+    private static TextMeshProUGUI ammoText;
+
+    private void Start()
+    {
+        ammoText = GameObject.Find("AmmoText").GetComponent<TextMeshProUGUI>();
+    }
+
+    public static void SetAmmoText(int ammo, int magazines)
+    {
+        ammoText.text = ammo.ToString() + " | " + magazines.ToString();
+    }
+}
